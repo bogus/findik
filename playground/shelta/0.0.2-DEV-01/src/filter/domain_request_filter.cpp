687ed9c52a5078e21f552952bc84719ca350bc89
shelta <shelta@d40773b4-ada0-11de-b0a2-13e92fe56a31>
2008-12-23 17:42:24 +0000

    Initialized 0.0.2-DEV-01 .

---
+++ playground/shelta/0.0.2-DEV-01/src/filter/domain_request_filter.cpp
@@ -0,0 +1,35 @@
+#include "domain_request_filter.hpp"
+
+namespace findik {
+	namespace filter {
+		
+		domain_request_filter::domain_request_filter(findik::persistency::dbmanager::pointer & manager, findik::io::request & request)
+			: findik::filter::abstract_request_filter(manager, request)
+		{
+		}
+
+		domain_request_filter::~domain_request_filter(void)
+		{
+		}
+
+		bool domain_request_filter::filter()
+		{
+			bool isOk = manager_->domainQuery(request_.host());
+			if(!isOk) {
+				LOG4CXX_WARN(logging::log_initializer::filter_logger, request_.host() << " DOMAIN BLOCKED");
+				filter_report_->generate_report(filter_report::request_domain,request_.host());
+			}
+			return isOk;
+		}
+		findik::filter::abstract_request_filter *domain_request_filter::construct(persistency::dbmanager::pointer & manager, io::request & request)
+		{
+			return new domain_request_filter(manager,request);
+		}
+
+		void init_domain_request_filter ()
+		{
+			static findik::filter::request_filter_factory_registrar<findik::filter::domain_request_filter> rffr;
+		}
+		
+	}
+}
---
+++ playground/shelta/0.0.2-DEV-01/src/logging/log_initializer.cpp
@@ -0,0 +1,61 @@
+#include "log_initializer.hpp"
+
+namespace findik {
+	namespace logging {
+
+		log_initializer::log_initializer(void)
+		{
+		}
+		
+		bool log_initializer::load_conf(std::string file_name)
+		{
+			conf_file = file_name;
+			try
+		{
+				log4cxx::PropertyConfigurator::configure(file_name);
+				LOG4CXX_INFO(user_logger, "Findik logging started");
+			}
+			catch(log4cxx::helpers::Exception&)
+		{	
+				return false;
+		}
+			return true;
+		}
+
+		bool log_initializer::reload_conf()
+		{	
+			if(conf_file==""){
+				LOG4CXX_ERROR(user_logger,"unable reload log configuration file, load_conf first");		
+				return false;
+			}
+
+			reset();
+			load_conf(conf_file);
+			return true;
+		}
+
+		void log_initializer::shutdown(){
+			log_initializer::reset();
+		}
+
+		void log_initializer::reset()
+		{	
+			LOG4CXX_INFO(user_logger,"Findik logging stopped");
+			log4cxx::Logger::getRootLogger()->removeAllAppenders();
+		}
+
+		log_initializer::~log_initializer(void)
+		{
+		}
+
+		//initialize static global application logger
+		log4cxx::LoggerPtr log_initializer::user_logger(log4cxx::Logger::getLogger("findik-log"));
+
+		//initialize static global filter logger
+		log4cxx::LoggerPtr log_initializer::filter_logger(log4cxx::Logger::getLogger("filter"));
+		
+		//initialize static global debug logger
+		log4cxx::LoggerPtr log_initializer::debug_logger(log4cxx::Logger::getLogger("findik"));
+	}
+}
+
