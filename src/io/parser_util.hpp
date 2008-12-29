dc3630666f95f253511284644072c51479abdb90
shelta <shelta@d40773b4-ada0-11de-b0a2-13e92fe56a31>
2008-12-29 19:48:16 +0000

    Merged with /playground/shelta/0.0.2-DEV-7 125:HEAD ; Added doxygen comments for findik::io namespace.

--- src/io/parser_util.hpp
+++ src/io/parser_util.hpp
@@ -1,4 +1,6 @@
 /*
+  Copyright (C) 2008 H. Kerem Cevahir (shelta) <findikmail@gmail.com>
+
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 2 of the License, or
@@ -22,105 +24,153 @@
 namespace findik {
 	namespace io {
 
-/// Check if a byte is an HTTP character.
+/*!
+A method to check if a byte is an HTTP character.
+
+\param c byte to be checked
+\return true if given byte is an HTTP character.
+*/
 static bool is_char(int c)
 {
-  return c >= 0 && c <= 127;
+	return c >= 0 && c <= 127;
 }
 
-/// Check if a byte is an HTTP control character.
+/*!
+Check if a byte is an HTTP control character.
+
+\param c byte to be checked
+\return true if given byte is an HTTP control character.
+*/
 static bool is_ctl(int c)
 {
-  return c >= 0 && c <= 31 || c == 127;
+	return c >= 0 && c <= 31 || c == 127;
 }
 
-/// Check if a byte is defined as an HTTP tspecial character.
+/*!
+Check if a byte is defined as an HTTP tspecial character.
+
+\param c byte to be checked
+\return true if given byte is defined as an HTTP tspecial character.
+*/
 static bool is_tspecial(int c)
 {
-  switch (c)
-  {
-  case '(': case ')': case '<': case '>': case '@':
-  case ',': case ';': case ':': case '\\': case '"':
-  case '/': case '[': case ']': case '?': case '=':
-  case '{': case '}': case ' ': case '\t':
-    return true;
-  default:
-    return false;
-  }
+	switch (c)
+	{
+		case '(': case ')': case '<': case '>': case '@':
+		case ',': case ';': case ':': case '\\': case '"':
+		case '/': case '[': case ']': case '?': case '=':
+		case '{': case '}': case ' ': case '\t':
+			return true;
+		default:
+			return false;
+	}
 }
 
-/// Check if a byte is a digit.
+/*!
+Check if a byte is a digit.
+
+\param c byte to be checked
+\return true if given byte is a digit.
+*/
 static bool is_digit(int c)
 {
-  return c >= '0' && c <= '9';
+	return c >= '0' && c <= '9';
 }
 
+/*!
+Check if a byte is a hex digit.
+
+\param c byte to be checked
+\return true if given byte is a hex digit.
+*/
 static bool is_hex(int c)
 {
-  if (is_digit(c) ||
-	  ( c >= 'a' && c <= 'f' ) ||
-	  ( c >= 'A' && c <= 'F' )
-	  )
-	  return true;
-  else
-    return false;
-
+	if (is_digit(c) ||
+		( c >= 'a' && c <= 'f' ) ||
+		( c >= 'A' && c <= 'F' )
+		)
+		return true;
+	else
+		return false;
 }
 
+/*!
+Converts given hex string to integer.
+
+\param hex hex string to convert.
+\return integer value of hex string.
+*/
 static int hex2int(std::string hex){
-// take a string which contains and int as hex and turn it into the int..
-// of course this won't work if you parse a non hex number string
-   std::stringstream ss(hex);
-   int i;
-   ss>>std::hex>>i;
-   return i;
+	std::stringstream ss(hex);
+	int i;
+	ss>>std::hex>>i;
+	return i;
 }
 
+/*!
+Check if a byte is defined as an HTTP URI unreserved character.
+
+\param c byte to be checked
+\return true if given byte is defined as an HTTP URI unreserved character.
+*/
 static bool is_uri_char_unreserved(int c)
 {
 	if (is_digit(c) || isalpha(c))
 		return true;
 	switch (c)
-    {
-    case '-': case '_': case '.': case '~':
-      return true;
-    }
+	{
+		case '-': case '_': case '.': case '~':
+		return true;
+	}
+
 	return false;
 }
 
+/*!
+Check if a byte is defined as an HTTP URI reserved character.
+
+\param c byte to be checked
+\return true if given byte is defined as an HTTP URI reserved character.
+*/
 static bool is_uri_char_reserved(int c) {
 	switch (c)
-    {
-    case '!': case '*': case '\'': case '(': case ')':
-	case ';': case ':': case '@': case '&': case '=':
-    case '+': case '$': case ',': case '?': case '/':
-	case '%': case '#': case '[': case ']':
-      return true;
-    }
+	{
+		case '!': case '*': case '\'': case '(': case ')':
+		case ';': case ':': case '@': case '&': case '=':
+		case '+': case '$': case ',': case '?': case '/':
+		case '%': case '#': case '[': case ']':
+		return true;
+	}
+
 	return false;
 }
 
+/*!
+Check if a byte is defined as an HTTP URI character.
+
+\param c byte to be checked
+\return true if given byte is defined as an HTTP URI character.
+*/
 static bool is_uri_char(int c)
 {
 	return is_uri_char_unreserved(c) || is_uri_char_reserved(c);
 }
 
+/*!
+Trims trailing and heading spaces of specified string.
 
+\param str string to be trimmed.
+*/
 static void trim_spaces( std::string& str)
 {
-    // Trim Both leading and trailing spaces
-    std::size_t startpos = str.find_first_not_of(" \t"); // Find the first character position after excluding leading blank spaces
-    std::size_t endpos = str.find_last_not_of(" \t"); // Find the first character position from reverse af
+    std::size_t startpos = str.find_first_not_of(" \t");
+    std::size_t endpos = str.find_last_not_of(" \t");
 
-    // if all spaces or empty return an empty string
     if(( std::string::npos == startpos ) || ( std::string::npos == endpos))
-    {
         str = "";
-    }
     else
         str = str.substr( startpos, endpos - startpos + 1 );
 }
-
 
 	}
 }
--- src/io/request.hpp
+++ src/io/request.hpp
@@ -1,4 +1,6 @@
 /*
+  Copyright (C) 2008 H. Kerem Cevahir (shelta) <findikmail@gmail.com>
+
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 2 of the License, or
@@ -26,60 +28,154 @@
 namespace findik {
 namespace io {
 
-/// A request received from a client.
+/*!
+A request received from a client.
+
+@author H. Kerem Cevahir (shelta)
+*/
 class request
 {
 public:
-  request();
 
-  enum method_type {
-	  get,
-	  post,
-	  head,
-	  put,
-	  delete_,
-	  trace,
-	  options,
-	  connect
-  } method;
+	/*!
+	Default constructor.
+	*/
+	request();
 
-  std::string uri;
-  unsigned int http_version_major;
-  unsigned int http_version_minor;
-  std::vector<header> headers;
+	/*!
+	Method of request.
+	*/
+	enum method_type {
+		get,
+		post,
+		head,
+		put,
+		delete_,
+		trace,
+		options,
+		connect
+	} method;
+	
+	/*!
+	Uri in HTTP request command line.
+	*/
+	std::string uri;
+	
+	/*!
+	Major number of HTTP Version in HTTP request command line.
+	*/
+	unsigned int http_version_major;
+	
+	/*!
+	Minor number of HTTP Version in HTTP request command line.
+	*/
+	unsigned int http_version_minor;
 
+	/*!
+	Headers of HTTP request.
+	*/
+	std::vector<header> headers;
+	
+	/*!
+	Gets destination host from headers of request.
 
+	\return host if "Host:" key has been found in headers, else "".
+	*/
+	const std::string & host();
 
-  const std::string & host();
-  unsigned int port ();
+	/*!
+	Gets port of destination host from headers of request.
 
-  const std::string & get_full_uri ();
-  const std::string & get_request_path ();
-  unsigned int content_length();
+	\return port if "Host:" key has been found in headers, else 80.
+	*/
+	unsigned int port ();
 
-  void to_streambuf(boost::asio::streambuf & sbuf_);
+	/*!
+	Gets full uri of request. This methods craft full uri from HTTP request command line and "Host:" header.
 
-  bool has_content();
+	\return full uri of request.
+	*/
+	const std::string & get_full_uri ();
 
-  const std::string & content_raw();
+	/*!
+	Gets request path. This methods gets request path from HTTP request command line.
 
-  void push_to_content(char input);
+	\return full uri of request.
+	*/
+	const std::string & get_request_path ();
+
+	/*!
+	Gets length of request content. Some HTTP request methods (POST) may contain data.
+
+	\return length of request content if "Content-Length" key has been found in headers, else 0.
+	*/
+	unsigned int content_length();
+
+	/*!
+	Crafts a byte stream from request. This method is used while sending request to remote endpoing (HTTP server).
+
+	\param sbuf_ stream buffer to push bytes into.
+	*/
+	void to_streambuf(boost::asio::streambuf & sbuf_);
+
+	/*!
+	Checks if request method supports data sending.
+
+	\return true if request method supports data sending, else false.
+	*/
+	bool has_content();
+
+	/*!
+	Gets raw content of request.
+
+	\return raw content.
+	*/
+	const std::string & content_raw();
+
+	/*!
+	Pushes character to raw content vector.
+
+	\param input char to be pushed.
+	*/
+	void push_to_content(char input);
 
 
 private:
 
+	/*!
+	Raw content.
+	\sa push_to_content(char input)
+	*/
 	std::string content_raw_;
 
-	boost::asio::streambuf content_;
-
+	/*!
+	Destination host of request.
+	\sa host()
+	*/
 	std::string host_;
 
+	/*!
+	Request path.
+	\sa get_request_path()
+	*/
 	std::string request_path_;
 
+	/*!
+	Full uri of request.
+	\sa get_full_uri()
+	*/
 	std::string full_uri_;
 
+	/*!
+	Port of destination endpoint.
+	\sa port()
+	*/
 	unsigned int port_;
 
+	/*!
+	Length of request content.
+	\sa content_length()
+	*/
 	unsigned int content_length_;
 };
 
