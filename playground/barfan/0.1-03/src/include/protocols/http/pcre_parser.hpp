1968e97fff7567cfe377f3dececfd2627818ebd4
barfan <barfan@d40773b4-ada0-11de-b0a2-13e92fe56a31>
2009-02-06 22:42:11 +0000

    pcre parser

---
+++ playground/barfan/0.1-03/src/include/protocols/http/pcre_parser.hpp
@@ -0,0 +1,73 @@
+/*
+  Copyright (C) 2008 Burak OGUZ (barfan) <findikmail@gmail.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef FINDIK_PROTOCOLS_HTTP_PCRE_PARSER_HPP
+#define FINDIK_PROTOCOLS_HTTP_PCRE_PARSER_HPP
+
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/shared_ptr.hpp>
+
+#include <pcrecpp.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include "mysqldbmanager.hpp"
+
+#include <string>
+
+namespace findik
+{
+	namespace protocols
+	{
+		namespace http
+		{
+			
+			/*!
+                        PCRE Parser class for regular expression operations.
+			@author Burak OGUZ (barfan)
+			 */
+			class pcre_parser
+			{
+				public:
+					pcre_parser();
+					~pcre_parser();
+					pcre_parser(int category_id_, std::string & pattern_);
+					pcre_parser(pcre_parser const &pcre_parser_);
+					pcrecpp::RE * get_re();
+					void set_re(pcrecpp::RE *re_);
+					int get_category_id();
+					void set_category_id(int category_id_);
+
+				private:
+					pcrecpp::RE * re_;
+					int category_id_;
+					pcrecpp::RE_Options *options_;
+			};
+
+			static std::vector<pcre_parser *> re_vector;
+
+			std::vector<pcre_parser *> & get_re_vector();
+
+			void generate_pcre_parser();
+
+		}
+	}
+}
+
+#endif
+
