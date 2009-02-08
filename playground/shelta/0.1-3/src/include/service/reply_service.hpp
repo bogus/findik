b23b80542ef1a5a78c49c1574483479045ea2c44
shelta <shelta@d40773b4-ada0-11de-b0a2-13e92fe56a31>
2009-02-08 18:26:48 +0000

    Initialized /playground/shelta/0.1-3

---
+++ playground/shelta/0.1-3/src/include/service/reply_service.hpp
@@ -0,0 +1,83 @@
+/*
+  Copyright (C) 2008 H. Kerem Cevahir (shelta) <findikmail@gmail.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef FINDIK_SERVICE_REPLY_SERVICE_HPP
+#define FINDIK_SERVICE_REPLY_SERVICE_HPP
+
+#define FC_BAD_LOCAL 400
+#define FC_BAD_REMOTE 500
+
+#include <boost/asio.hpp>
+#include <boost/noncopyable.hpp>
+#include <map>
+
+#include "protocol.hpp"
+#include "filter_reason.hpp"
+
+namespace findik
+{
+	namespace service
+	{
+		/*!
+		Service for getting prepared replies.
+                \extends boost::noncopyable this class has designed to be not copyable.
+		*/
+		class reply_service :
+                        private boost::noncopyable
+		{
+		public:
+			/*!
+			Default constructor.
+			*/
+			reply_service();
+
+			/*!
+			Destructor.
+			*/
+			~reply_service();
+
+			/*!
+			Crafts request reply into given stream buffer.
+
+			\param sbuf buffer to populate reply into. 
+			\param proto protocol of reply.
+			\param reason deny reason of a filter.
+			*/
+			void reply(boost::asio::streambuf & sbuf, findik::io::protocol proto, findik::filter::filter_reason_ptr reason);
+
+			/*!
+			Crafts request reply into given stream buffer.
+
+			\param sbuf buffer to populate reply into. 
+			\param proto protocol of reply.
+			\param code reply code.
+			*/
+			void reply(boost::asio::streambuf & sbuf, findik::io::protocol proto, unsigned int code);
+
+		protected:
+			/*!
+			Map to store stock replies.
+			*/
+			std::map<findik::io::protocol, std::map<unsigned int, std::string> > stock_replies_;
+
+		};
+	}
+}
+
+#endif
+
