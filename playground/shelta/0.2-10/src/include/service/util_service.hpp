494a468f88ef85c3980e36668b642fd0eefbdb07
shelta <shelta@d40773b4-ada0-11de-b0a2-13e92fe56a31>
2009-04-07 21:12:15 +0000

    Initialized /playground/shelta/0.2-10 .

---
+++ playground/shelta/0.2-10/src/include/service/util_service.hpp
@@ -0,0 +1,83 @@
+/*
+  Copyright (C) 2008 H. Kerem Cevahir (shelta) <findikmail@gmail.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef FINDIK_SERVICE_UTIL_SERVICE_HPP
+#define FINDIK_SERVICE_UTIL_SERVICE_HPP
+
+#include <boost/noncopyable.hpp>
+
+#include "pcre_service.hpp"
+#include "magic_num_service.hpp"
+
+namespace findik
+{
+	namespace service
+	{
+		/*!
+		Container to manage various utilities. 
+                \extends boost::noncopyable this class has designed to be not copyable.
+		*/
+		class util_service :
+                        private boost::noncopyable
+		{
+		public:
+			/*!
+			Default constructor.
+			*/
+			util_service();
+
+			/*!
+			Destructor.
+			*/
+			~util_service();
+
+			/*!
+			Method to access pcre service.
+			\returns pcre_service instance.
+			*/
+			pcre_service & pcre();
+
+			/*!
+                        Method to access magic number service.
+                        \returns magic_number_service instance.
+                        */
+                        magic_num_service & magic_num();
+
+			/*!
+			Method to initialize utilities.
+			*/
+			void start();
+
+		protected:
+
+			/*!
+			Pcre service instance.
+			*/
+			pcre_service pcre_srv_;
+
+			/*!
+                        Magic number service instance.
+                        */
+                        magic_num_service magic_num_srv_;
+
+		};
+	}
+}
+
+#endif
+
